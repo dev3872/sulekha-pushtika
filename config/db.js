c4783843ddbba67c8c6b4a5315f81dda1f8b4879
dev3872 <45680062+dev3872@users.noreply.github.com>
2021-01-24 00:00:16 +0530

    Database Init

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const db =
+  "mongodb+srv://dbUser:dbUser@cluster0.xufbb.mongodb.net/sulekh-pushtika?retryWrites=true&w=majority";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true,
+      retryWrites: false,
+    });
+
+    console.log("MongoDB Connected...");
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
--- server.js
+++ server.js
@@ -1,27 +1,50 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+/**
+ * Database Setup
+ */
+const connectDB = require("./config/db");
+connectDB();
+
+/**
+ *  Express initialization
+ */
 const app = express();
-const PORT = 4000;
+
+/**
+ * Port Initialization
+ */
+const port = 4000;
+
+/**
+ * Middleware Setup
+ */
+app.use(express.json());
 
 /*
-Route: '/'
-Type: GET
-Description: Server Route to root directory at port 4000
-*/
+ * Route: '/'
+ * Type: GET
+ * Description: Server Route to root directory at port 4000
+ */
 
-app.get("/", (req, res) => {
-  res.json({
-    test: "working",
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
-});
+}
 
 /*
 Start server at PORT 4000
 */
 
-app.listen(PORT, (err) => {
-  if (err) {
-    console.error(err);
+app.listen(process.env.PORT || port, (err) => {
+  if (!err) {
+    console.log(`Server started at port ${port}`);
   } else {
-    console.info(`server running at Port ${PORT}`);
+    console.log(err);
   }
 });
