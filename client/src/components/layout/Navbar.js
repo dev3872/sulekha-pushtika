d00bcbb0b1428ea0ec0d1616ab71f35bde631f81
dev3872 <45680062+dev3872@users.noreply.github.com>
2021-01-25 02:52:34 +0530

    Navbar Designed

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,26 @@
-import React from "react";
-
+import React, { useRef } from "react";
+import "../../styles/layout/navbar.scss";
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const navbarUserOptionsRef = useRef();
+  const focusNavbarUserAccount = () => {
+    console.log(navbarUserOptionsRef);
+    navbarUserOptionsRef.current.style.top == "-39vh"
+      ? (navbarUserOptionsRef.current.style.top = "9vh")
+      : (navbarUserOptionsRef.current.style.top = "-39vh");
+  };
+
+  return (
+    <div className="navbar">
+      <span className="navbar-logo" />
+      <span className="navbar-brand" />
+      <span className="navbar-user-account" onClick={focusNavbarUserAccount} />
+      <div ref={navbarUserOptionsRef} className="navbar-user-options">
+        <span className="item">Login</span>
+        <span className="item">Register</span>
+        <span className="item">Help</span>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
